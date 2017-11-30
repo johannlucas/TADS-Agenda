0accc7d2fb2e5e3b4b4bb2e016ced1e7e779ecdd
Gustavo F. Uessler <gustavouessler@gmail.com>
2017-11-30 12:57:07 -0200

    Update Arquivo.java

--- src/agenda/Arquivo.java
+++ src/agenda/Arquivo.java
@@ -1,60 +1,56 @@
 package agenda;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.util.Scanner;
 
-public class Arquivo {
+public class Agenda {
 
-    private String path;
-    private Agenda content;
+    private String id;
+    private String nome;
+    private String telefone;
 
-    private static RandomAccessFile arquivo;
-
-    public Arquivo(String path, Agenda content) {
-        this.path = path;
-        this.content = content;
+    public Agenda(String id, String nome, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
     }
 
-    public void Buscar() throws FileNotFoundException, IOException {
+    //Construtor para ler
+    public Agenda(String id) {
+        this.id = id;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    @Override
+    public String toString() {
         
-        BufferedReader buff = new BufferedReader(new FileReader(this.path));
-        
-        String line;
-        while ((line = buff.readLine().trim()) != null) {
-            if (line.equals(String.valueOf(this.content.getId()))) {
-                System.out.println("Id: " + line);
-                System.out.println("Nome: " + buff.readLine().trim());
-                System.out.println("Telefone: " + buff.readLine().trim());
-
-                break;
-            }
-        }
-    }
-
-    public void Inserir() throws FileNotFoundException, IOException {
-
-        DataOutputStream stream = new DataOutputStream(
-                new FileOutputStream(this.path, true));
-
-        stream.writeUTF(this.content.getId() + "\n");
-        stream.writeUTF(this.content.getNome() + "\n");
-        stream.writeUTF(this.content.getTelefone() + "\n");
-
-        stream.flush();
-        stream.close();
-
-        System.out.println("Arquivo escrito com sucesso!");
-    }
-
-    public void Deletar() {
-
+        return this.id
+                + "\n"
+                + this.nome
+                + "\n"
+                + this.telefone
+                + "\n";
     }
 }
